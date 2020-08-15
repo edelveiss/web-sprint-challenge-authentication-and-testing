96c3a5bccb075f50d1855bff797622ea04e15200
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-08-14 18:17:35 -0700

    Dependencies are installed and knexfile and dbConfig file are configured

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,43 @@
+require("dotenv").config();
+const pgConnection =
+  process.env.DATABASE_URL || "postgresql://postgres@localhost/jokes";
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/auth.db3" },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations",
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" },
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/test.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
   },
 };
